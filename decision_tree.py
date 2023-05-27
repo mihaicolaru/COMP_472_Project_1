397d372ef6debc450001f95c8a8d8493e906dbb5
Mihai Olaru <mihai.c.olaru@gmail.com>
2023-05-27 19:19:14 -0400

    got the example code working with our values, prediction achieved from input attribute set

--- decision_tree.py
+++ decision_tree.py
@@ -15,28 +15,39 @@ import graphviz
 # TODO: functions
 
 # TODO: main
+
+# Alt (Alternate): other suitable alternatives nearby   0: No   1: Yes
+# Bar: has bar waiting area                             0: No   1: Yes
+# Fri (Fri/Sat): is Friday or Saturday                  0: No   1: Yes
+# Hun (Hungry): is hungry                               0: No   1: Yes
+# Pat (Patrons): how many patrons ()                    0: None 1: Some 2: Full
+# Price: price range                                    0: $    1: $$   2: $$$
+# Rain (Raining): is raining outsidn                    0: No   1: Yes
+# Res (Reservation): reservation was made               0: No   1: Yes
+# Type: what kind of restaurant                         0: French   1: Italian  2: Thai 3: Burger
+# Est (WaitEstimate): host wait estimate                0: 0-10     1: 10-30    2: 30-60    3: >60
+# WillWait (output):                                    0: No   1: Yes
+
 dataset = np.array([
-    ['Yes', 'No', 'No', 'Yes', 'Some', '$$$', 'No', 'Yes', 'French', '0-10', 'Yes'],
-    ['Yes', 'No', 'No', 'Yes', 'Full', '$$', 'No', 'No', 'Thai', '30-60', 'No'],
-    ['No', 'Yes', 'No', 'No', 'Some', '$', 'No', 'No', 'Burger', '0-10', 'Yes'],
-    ['Yes', 'No', 'Yes', 'Yes', 'Full', '$', 'Yes', 'No', 'Thai', '10-30', 'Yes'],
-    ['Yes', 'No', 'Yes', 'No', 'Full', '$$$', 'No', 'Yes', 'French', '>60', 'No'],
-    ['No', 'Yes', 'No', 'Yes', 'Some', '$$', 'Yes', 'Yes', 'Italian', '0-10', 'Yes'],
-    ['No', 'Yes', 'No', 'No', 'None', '$', 'Yes', 'No', 'Burger', '0-10', 'No'],
-    ['No', 'No', 'No', 'Yes', 'Some', '$$', 'Yes', 'Yes', 'Thai', '0-10', 'Yes'],
-    ['No', 'Yes', 'Yes', 'No', 'Full', '$', 'Yes', 'No', 'Burger', '>60', 'No'],
-    ['Yes', 'Yes', 'Yes', 'Yes', 'Full', '$$$', 'No', 'Yes', 'Italian', '10-30', 'No'],
-    ['No', 'No', 'No', 'No', 'None', '$', 'No', 'No', 'Thai', '0-10', 'No'],
-    ['Yes', 'Yes', 'Yes', 'Yes', 'Full', '$', 'No', 'No', 'Burger', '30-60', 'Yes'],
+    [1, 0, 0, 1, 1, 2, 0, 1, 0, 0, 1],
+    [1, 0, 0, 1, 2, 0, 0, 0, 2, 2, 0],
+    [0, 1, 0, 0, 1, 0, 0, 0, 3, 0, 1],
+    [1, 0, 1, 1, 2, 0, 1, 0, 2, 1, 1],
+    [1, 0, 1, 0, 2, 2, 0, 1, 0, 3, 0],
+    [0, 1, 0, 1, 1, 1, 1, 1, 1, 0, 1],
+    [0, 1, 0, 0, 0, 0, 1, 0, 3, 0, 0],
+    [0, 0, 0, 1, 1, 1, 1, 1, 2, 0, 1],
+    [0, 1, 1, 0, 2, 0, 1, 0, 3, 3, 0],
+    [1, 1, 1, 1, 2, 2, 0, 1, 1, 1, 0],
+    [0, 0, 0, 0, 0, 0, 0, 0, 2, 0, 0],
+    [1, 1, 1, 1, 2, 0, 0, 0, 3, 2, 1],
 ])
-print(dataset)
+# print(dataset)
 
 df2 = pd.DataFrame(dataset,
                    columns=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est', 'WillWait'])
 blankIndex=[''] * len(df2)
 df2.index=blankIndex
-
-df2
 
 X = dataset[:, 0:10]
 y = dataset[:, 10]
@@ -47,46 +58,59 @@ y = le.fit_transform(y)
 
 dtc = tree.DecisionTreeClassifier(criterion="entropy")
 
-# dtc.fit(X, y)
-
-# y_pred = dtc.predict([])
-# print("Predicted output: ", le.inverse_transform(y_pred))
-# tree.plot_tree(dtc)
-
-# dot_data = tree.export_graphviz(dtc, out_file=None,
-# feature_names=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est'],
-# class_names=le.classes_,
-# filled=True, rounded=True)
-# graph = graphviz.Source(dot_data)
-# graph.render("mytree1")
+# train model on given attribute matrix
+dtc.fit(X, y)
 
 
-# example 2 in the tutorial
-from sklearn.datasets import load_iris
-from sklearn.model_selection import train_test_split
-# load the Iris dataset
-iris = load_iris()
-X, y = iris.data, iris.target
-X_train, X_test, y_train, y_test = train_test_split(
-X, y, test_size=0.6, random_state=0)
-# create and print the decision tree
-dtc = tree.DecisionTreeClassifier(criterion="entropy")
-dtc.fit(X_train, y_train)
+# print trained model
 tree.plot_tree(dtc)
 
 dot_data = tree.export_graphviz(dtc, out_file=None,
-feature_names=iris.feature_names,
-class_names=iris.target_names,
+feature_names=['Alt', 'Bar', 'Fri', 'Hun', 'Pat', 'Price', 'Rain', 'Res', 'Type', 'Est'],
+# class_names=le.classes_,
 filled=True, rounded=True,
 special_characters=True)
 graph = graphviz.Source(dot_data)
-graph.render("iristree")
+graph.render("mytree1")
 
-y_pred = dtc.predict(X_test)
-from sklearn.metrics import classification_report
-print(classification_report(y_test, y_pred))
 
-from sklearn.metrics import confusion_matrix
-print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
+# input set of attributes
+y_pred = dtc.predict([[0, 0, 1, 1, 0, 0, 1, 1, 0, 0]])
 
-# this is all stuff taken from the tutorials
+# output prediction based on input set
+print("Predicted output: ", le.inverse_transform(y_pred))
+
+
+
+
+
+
+
+
+# example 2 in the tutorial
+# from sklearn.datasets import load_iris
+# from sklearn.model_selection import train_test_split
+# # load the Iris dataset
+# iris = load_iris()
+# X, y = iris.data, iris.target
+# X_train, X_test, y_train, y_test = train_test_split(
+# X, y, test_size=0.6, random_state=0)
+# # create and print the decision tree
+# dtc = tree.DecisionTreeClassifier(criterion="entropy")
+# dtc.fit(X_train, y_train)
+# tree.plot_tree(dtc)
+
+# dot_data = tree.export_graphviz(dtc, out_file=None,
+# feature_names=iris.feature_names,
+# class_names=iris.target_names,
+# filled=True, rounded=True,
+# special_characters=True)
+# graph = graphviz.Source(dot_data)
+# graph.render("iristree")
+
+# y_pred = dtc.predict(X_test)
+# from sklearn.metrics import classification_report
+# print(classification_report(y_test, y_pred))
+
+# from sklearn.metrics import confusion_matrix
+# print("Confusion Matrix:\n", confusion_matrix(y_test, y_pred))
